6fe1b786d8595b0896a0e6a887edb92b52aa5175
Mark Burton <mark.burton@zither-it.co.uk>
2025-01-21 20:40:23 +0000

    Add console app

---
+++ src/FuzzConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FuzzLib;
+
+Console.WriteLine("Hello, World!");
+
+// Example inputs for fuzz testing 
+string[] testInputs = { "123.45", "abc", "0", "-456.78", "1.7976931348623157E+308", null }; 
+var fuzzClass = new FuzzClass();
+
+foreach (var input in testInputs) { 
+    Console.WriteLine("Testing input: " + input); 
+    fuzzClass.FuzzMe(input); 
+    Console.WriteLine(); 
+    }
